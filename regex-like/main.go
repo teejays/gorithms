332941d7d30ebdf476ecc8c0b3adc264e9fa0228
teejays <github@teejay.me>
2018-07-29 01:14:49 -0400

    more efficient pointers

--- regex-like/main.go
+++ regex-like/main.go
@@ -18,72 +18,92 @@ func match(_pattern, _str string) bool {
 
 	print(INFO, "Starting matching: pattern='%s', string='%s'", _pattern, _str)
 
-	for i := 0; i <= len(pattern); i++ {
-		memo[i] = make([]bool, len(str)+1)
+	var i, j, _i, _j int
+	for {
 
-		for j := 0; j <= len(str); j++ {
+		print(DEBUG, "Raw Indexes: i=%d, j=%d", i, j)
 
-			print(DEBUG, "Matching: pattern='%s', string='%s'", string(pattern[0:i]), string(str[0:j]))
-			print(DEBUG, "Populating Memo: i=%d, j=%d", i, j)
-
-			// Both pattern and string are empty
-			if i == 0 && j == 0 {
-				print(DEBUG, "-- empty pattern & str => %t", true)
-				memo[i][j] = true
-
-			} else if i == 0 {
-				// pattern is empty
-				print(DEBUG, "-- empty pattern but non-empty string => %t", false)
-				memo[i][j] = false
-
-			} else if j == 0 {
-				// string is empty
-				print(DEBUG, "-- empty string => ...")
-
-				// the pattern has even chars, and the previous pattern char was ? or *
-				if i%2 == 0 && (pattern[i-2] == '?' || pattern[i-2] == '*') {
-					print(DEBUG, "-- empty string but it's optional => ...")
-					memo[i][j] = memo[i-2][j]
-				}
-
-			} else if isSpecialRepeating(pattern[i-1]) {
-				// if the current pattern char requires a following char
-				print(DEBUG, "-- special character, go to next...")
-
-			} else if pattern[i-1] == str[j-1] || pattern[i-1] == '.' {
-				// if the chars are same (works for first char as well)
-				print(DEBUG, "-- current characters matched...")
-
-				// if previous was a special char
-				if i > 1 && (pattern[i-2] == '?' || pattern[i-2] == '*') {
-					memo[i][j] = (memo[i][j-1] || memo[i-2][j-1] || memo[i-2][j])
-				} else if i > 1 && pattern[i-2] == '+' {
-					memo[i][j] = (memo[i][j-1] || memo[i-2][j-1])
-				} else {
-					memo[i][j] = memo[i-1][j-1]
-				}
-
-			} else {
-				// if chars are not the same, and not special
-
-				if i == 1 {
-
-				} else if pattern[i-2] == '+' {
-					// if previous pattern char is +
-
-				} else if pattern[i-2] == '?' || pattern[i-2] == '*' {
-					// if previous pattern char is ? or *
-					memo[i][j] = memo[i-2][j]
-				}
-			}
-
-			if memo[i][j] {
-				print(INFO, "Subpattern Match => %t", memo[i][j])
-			} else {
-				print(DEBUG, "Subpattern Match => %t", memo[i][j])
-			}
-
+		if i > len(pattern) {
+			break
 		}
+		if j > len(str) {
+			i++
+			j = 0
+			continue
+		}
+		_i, _j = i, j
+
+		if memo[i] == nil {
+			memo[i] = make([]bool, len(str)+1)
+		}
+
+		print(DEBUG, "Cleaned Indexes: i=%d, j=%d", i, j)
+		print(DEBUG, "Matching: pattern='%s', string='%s'", string(pattern[0:i]), string(str[0:j]))
+
+		// Both pattern and string are empty
+		if i == 0 && j == 0 {
+			print(DEBUG, "-- empty pattern & str")
+			memo[i][j] = true
+			_i = i + 1
+
+		} else if i == 0 {
+			// pattern is empty
+			print(DEBUG, "SHOULD NOT HAPPEN -- empty pattern but non-empty string => %t", false)
+			memo[i][j] = false
+			_j = j + 1
+
+		} else if j == 0 {
+			// string is empty
+			print(DEBUG, "-- empty string")
+
+			// the pattern has even chars, and the previous pattern char was ? or *
+			if i%2 == 0 && (pattern[i-2] == '?' || pattern[i-2] == '*') {
+				print(DEBUG, "-- empty string but it's optional => ...")
+				memo[i][j] = memo[i-2][j]
+			}
+			_j = j + 1
+
+		} else if pattern[i-1] == '?' || pattern[i-1] == '*' || pattern[i-1] == '+' {
+			// if the current pattern char requires a following char
+			print(DEBUG, "-- special character, go to next...")
+			_i = i + 1
+			_j = 0
+
+		} else if pattern[i-1] == str[j-1] || pattern[i-1] == '.' {
+			// if the chars are same (works for first char as well)
+			print(DEBUG, "-- current characters matched...")
+
+			// if previous was a special char
+			if i > 1 && (pattern[i-2] == '?' || pattern[i-2] == '*') {
+				memo[i][j] = (memo[i][j-1] || memo[i-2][j-1] || memo[i-2][j])
+			} else if i > 1 && pattern[i-2] == '+' {
+				memo[i][j] = (memo[i][j-1] || memo[i-2][j-1])
+			} else {
+				memo[i][j] = memo[i-1][j-1]
+			}
+			_j = j + 1
+
+		} else {
+			// if chars are not the same, and not special
+			if i > 1 && pattern[i-2] == '+' {
+				// if previous pattern char is +
+
+			} else if i > 1 && (pattern[i-2] == '?' || pattern[i-2] == '*') {
+				// if previous pattern char is ? or *
+				memo[i][j] = memo[i-2][j]
+			}
+			_j = j + 1
+		}
+
+		if memo[i][j] {
+			print(INFO, "Subpattern Match => %t", memo[i][j])
+		} else {
+			print(DEBUG, "Subpattern Match => %t", memo[i][j])
+		}
+
+		i = _i
+		j = _j
+
 	}
 
 	print(DEBUG, "Memo: %v", memo)
